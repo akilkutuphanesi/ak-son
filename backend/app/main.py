7599f47f0b385153f3d6b14dccbb555ef96e46fb
nildemirell <167444963+nildemirell@users.noreply.github.com>
2026-03-25 14:34:46 +0300

    gorsel sorunu güncel

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,7 +14,7 @@ app = FastAPI(title="Akıl Kütüphanesi")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,  # <-- BURASI DÜZELTİLDİ
     allow_methods=["*"],
     allow_headers=["*"],
 )
